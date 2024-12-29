5546f98dd8d32a9a5e9f2ca7ab79e64acfb0a0cf
tranduc2601 <breakgamer2601@gmail.com>
2024-12-29 08:50:46 +0700

    Add files via upload

---
+++ btc12.5.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool kiemTraSoNguyenTo(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int so1, so2;
+    printf("Nhap hai so nguyen: ");
+    scanf("%d %d", &so1, &so2);
+
+    if (kiemTraSoNguyenTo(so1)) {
+        printf("%d la so nguyen to.\n", so1);
+    } else {
+        printf("%d khong phai la so nguyen to.\n", so1);
+    }
+
+    if (kiemTraSoNguyenTo(so2)) {
+        printf("%d la so nguyen to.\n", so2);
+    } else {
+        printf("%d khong phai la so nguyen to.\n", so2);
+    }
+
+    return 0;
+}
+
